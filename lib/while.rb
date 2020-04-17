63077efce1940272d52273c78b4496f54ab59be0
Maysoon Tabali <mtab03@me.com>
2020-04-17 02:02:15 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,18 +1,14 @@
-require_relative "../lib/until.rb"
-require_relative "../lib/while.rb"
+#def using_while
+  #levitation_force = 6
+
+  #while levitation force is < 10
+    #puts "Wingardium Leviosa."#y
+   #levitation_force += 1
+#end
 
 def using_while
-  levitation_force = 6
-
-  while levitation force is < 10
-    puts "Wingardium Leviosa."#y
-   levitation_force += 1
-end
-
-def using_while
-	levitation_force = 6		 levitation_force = 6
-
-    while levitation_force < 10
+	levitation_force = 6
+  while levitation_force < 10
 	     puts "Wingardium Leviosa"
       levitation_force += 1
    end
