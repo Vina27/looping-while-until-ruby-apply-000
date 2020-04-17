306f4bf69095ef30b769fd139a21d189f211bac0
Maysoon Tabali <mtab03@me.com>
2020-04-17 02:06:28 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -6,11 +6,10 @@
    #levitation_force += 1
 #end
 
-def using_while
-	levitation_force = 6
-  
-  while levitation_force < 10
-	     puts "Wingardium Leviosa"
-      levitation_force += 1
-   end
-end	end
+def using_until
+  levitation_force = 6
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
+end 
